b9d85624f80e3fba269f5a13672a15e49f30a68f
Kurtis Edwards <edwards.kurtis@gmail.com>
2022-10-20 17:34:37 -0400

    add content and styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-//THIS IS WHERE YOUR JS GOES
+// QUOTE GENERATOR
+
+const generateQuote = function () {
+  const quotes = [
+    {
+      quote: "First, solve the problem. Then, write the code.",
+      author: " - John Johnson",
+    },
+    {
+      quote: "Make it work, make it right, make it fast.",
+      author: " - Kent Beck",
+    },
+    {
+      quote: "Testing leads to failure, and failure leads to understanding.",
+      author: " - Burt Rutan",
+    },
+    {
+      quote: "It's harder to read code than to write it.",
+      author: " - Joel Spolsky",
+    },
+  ];
+  let arrayIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quotes").innerHTML = quotes[arrayIndex].quote;
+  document.getElementById("author").innerHTML = quotes[arrayIndex].author;
+};
+window.onload = function () {
+  generateQuote();
+  setInterval(generateQuote, 5000);
+};
