1f8d6f5533fb5b919bdcb87c78bd67a7e911daa0
Kurtis Edwards <edwards.kurtis@gmail.com>
2022-11-14 21:05:51 -0500

    add css fade and js functions

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
 // QUOTE GENERATOR
+const quotes = [
+  {
+    quote: "First, solve the problem. Then, write the code.",
+    author: " - John Johnson",
+  },
+  {
+    quote: "Make it work, make it right, make it fast.",
+    author: " - Kent Beck",
+  },
+  {
+    quote: "Testing leads to failure, and failure leads to understanding.",
+    author: " - Burt Rutan",
+  },
+  {
+    quote: "It's harder to read code than to write it.",
+    author: " - Joel Spolsky",
+  },
+];
+let arrayIndex = 0;
+function cycleArray() {
+  let array = quotes[arrayIndex];
+  document.getElementById("quotes").innerHTML = array.quote;
+  document.getElementById("author").innerHTML = array.author;
 
-const generateQuote = function () {
-  const quotes = [
-    {
-      quote: "First, solve the problem. Then, write the code.",
-      author: " - John Johnson",
-    },
-    {
-      quote: "Make it work, make it right, make it fast.",
-      author: " - Kent Beck",
-    },
-    {
-      quote: "Testing leads to failure, and failure leads to understanding.",
-      author: " - Burt Rutan",
-    },
-    {
-      quote: "It's harder to read code than to write it.",
-      author: " - Joel Spolsky",
-    },
-  ];
-  let arrayIndex = Math.floor(Math.random() * quotes.length);
-  document.getElementById("quotes").innerHTML = quotes[arrayIndex].quote;
-  document.getElementById("author").innerHTML = quotes[arrayIndex].author;
-};
+  arrayIndex++;
+
+  if (arrayIndex === quotes.length) {
+    arrayIndex = 0;
+  }
+}
 window.onload = function () {
-  generateQuote();
-  setInterval(generateQuote, 5000);
+  cycleArray();
+  setInterval(cycleArray, 7000);
 };
